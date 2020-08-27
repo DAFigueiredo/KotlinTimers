e9ed8d2068c31af579d5b79be7d0839e6a5750a4
Diogo Figueiredo <diogo.alvesfigueiredo@gmail.com>
2020-08-27 17:22:14 +0100

    timer and toast

--- app/src/main/java/com/example/kotlintimers/MainActivity.kt
+++ app/src/main/java/com/example/kotlintimers/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.kotlintimers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+
+                textView.text = "Left: " + p0 / 1000
+            }
+
+            override fun onFinish() {
+
+                Toast.makeText(applicationContext, "Time is over!", Toast.LENGTH_LONG).show()
+                textView.text = "Left: 0"
+            }
+
+        }.start()
+
     }
 }
